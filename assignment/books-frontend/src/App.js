2af04752df12adb83d0ef1bdb21ff4eb094bbb34
Yusuf Khan <yusuf.khan1000098@gmail.com>
2022-02-12 15:14:50 -0500

    Added 404 page

--- assignment/books-frontend/src/App.js
+++ assignment/books-frontend/src/App.js
@@ -7,6 +7,7 @@ import NewBookPage from "./pages/NewBookPage/NewBookPage";
 import EditBookPage from "./pages/EditBookPage/EditBookPage";
 import { Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="/books" element={<ListingPage />} />
             <Route path="/books/new" element={<NewBookPage />} />
             <Route path="/books/:id" element={<EditBookPage />} />
+            <Route path="*" element={<NotFoundPage/>}/>
           </Routes>
         </AlertProvider>
       </HashRouter>
---
+++ assignment/books-frontend/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./NotFoundPage.module.css";
+
+const NotFoundPage = () => {
+  return (
+    <div className={styles.notFoundContainer}>This page doesn't exist. Looks like you're lost!</div>
+  );
+};
+
+export default NotFoundPage;
