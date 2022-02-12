3223b84fefe2b2704f6059fc2a49952419c44605
Yusuf Khan <yusuf.khan1000098@gmail.com>
2022-02-12 16:21:27 -0500

    Handled invalid route for rest of the routes

--- assignment/books-backend/routes/index.js
+++ assignment/books-backend/routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/**Handle invalid route*/
+router.use("/", (req, res) => {
+  res.status(404).json({ message: "Route does not exist" });
 });
 
 module.exports = router;
